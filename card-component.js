c0a50b253eac507ab18affdd82f41dd309442d18
Didi110605 <150129924+Didi110605@users.noreply.github.com>
2024-12-04 22:48:53 +0300

    Update card-component.js

--- card-component.js
+++ card-component.js
@@ -4,13 +4,11 @@ class CardComponent extends HTMLElement {
         const shadow = this.attachShadow({ mode: 'open' });
         const card = document.createElement('div');
         
-        // Добавление заголовка
         const header = this.querySelector('template[slot="header"]');
         if (header) {
             card.appendChild(header.content.cloneNode(true));
         }
 
-        // Добавление контента
         const content = this.querySelector('template[slot="content"]');
         if (content) {
             card.appendChild(content.content.cloneNode(true));
@@ -22,5 +20,4 @@ class CardComponent extends HTMLElement {
 
 customElements.define('card-component', CardComponent);
 
-// Определяем новый элемент
 customElements.define('card-component', CardComponent);
